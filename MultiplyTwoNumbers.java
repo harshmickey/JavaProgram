fcc5fbd4b25eb948fdad3fdd8dfc059bb07fe8ec
Harsh <harshmickey@gmail.com>
2016-11-30 12:04:09 +0530

    wrote a program to multiply two given numbers

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,10 @@
+//A Very Simple Example
+class MultiplyTwoNumbers {
+  public static void main(String[] args){
+  	int firstNumber, secondNumber, product;
+  	firstNumber = 10;
+  	secondNumber = 100;
+  	product = firstNumber*secondNumber;
+    System.out.println(product);
+  }
+}
